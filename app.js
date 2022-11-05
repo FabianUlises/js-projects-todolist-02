120ab119a9347d4b45136475e0aa8ac9380bac8e
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-11-05 12:00:39 -0700

    Added function to load localstorage tasks on page load

--- app.js
+++ app.js
@@ -3,15 +3,82 @@ const taskCtn = document.querySelector('.task-list');
 const addTaskBtn = document.querySelector('#new-taskBtn');
 const taskInput  = document.querySelector('#new-task');
 const button = document.querySelector('.edit');
+// Saving task to localstorage
 const saveLocalTasks = (task) => {
+    // Initialize tasks
+    let tasks;
+    // If localstorage doesnt contain task data
+    if(localStorage.getItem('tasks') === null) {
+        // Tasks = empty array 
+        tasks = [];
+        // If tasks isnt empty
+    } else {
+        // Set tasks = to localstorage tasks
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    // Add new task to tasks array
+    tasks.push(task);
+    // Update localstorage with new task
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+const loadLocalTasks = () => {
     let tasks;
     if(localStorage.getItem('tasks') === null) {
         tasks = [];
     } else {
         tasks = JSON.parse(localStorage.getItem('tasks'));
     }
-    tasks.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    tasks.forEach((task) => {
+        // Creating elements for single task and adding classes
+        const task_el = document.createElement('div');
+        task_el.classList.add('tasks-ctn');
+        const task_content_el = document.createElement('div');
+        task_content_el.classList.add('task');
+        const task_input_el = document.createElement('input');
+        task_input_el.classList.add('task-text');
+        task_input_el.type = 'text';
+        task_input_el.setAttribute('readonly', 'readonly');
+        task_input_el.value = task;
+        // Saveing task to localstorage
+        task_content_el.appendChild(task_input_el);
+        task_el.appendChild(task_content_el);
+        const task_action_el = document.createElement('div');
+        task_action_el.classList.add('task-actions');
+        const task_edit_el = document.createElement('button');
+        task_edit_el.classList.add('edit');
+        task_edit_el.innerHTML = 'Edit';
+        const task_delete_el = document.createElement('button');
+        task_delete_el.classList.add('delete');
+        task_delete_el.innerHTML = 'Delete';
+        // Appending elements to page
+        task_action_el.appendChild(task_edit_el);
+        task_action_el.appendChild(task_delete_el);
+        task_content_el.appendChild(task_action_el);
+        taskCtn.appendChild(task_el);
+        // Adding event handling to edit button
+        task_edit_el.addEventListener('click', () => {
+            // If the buttons text is 'edit'
+            if(task_edit_el.innerText.toLowerCase() == 'edit') {
+                // Remove readonly attr
+                task_input_el.removeAttribute('readonly')
+                // Auto focus on input
+                task_input_el.focus();
+                // Replace text from 'edit' to 'save'
+                task_edit_el.innerText = 'Save';
+                // Else text shoulbe be == 'save'; then run
+            } else {
+                // Add readonly attr
+                task_input_el.setAttribute('readonly', 'readonly');
+                // Replace text from 'save' to 'edit'
+                task_edit_el.innerText = 'Edit';
+            }
+        });
+        // Adding event handling to delete button
+        task_delete_el.addEventListener('click', () => {
+            // Remove task from task container
+            taskCtn.removeChild(task_el)
+        });
+    })
 }
 const createTask = (e) => {
     if(e.keyCode == 13) {
@@ -73,6 +140,7 @@ const createTask = (e) => {
         });
     }
 }
+document.addEventListener('DOMContentLoaded', loadLocalTasks);
 taskInput.addEventListener('keyup', (e) => {
     createTask(e);
 });
